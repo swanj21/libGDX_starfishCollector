5595715543b863b0f369d81751bdaf7cd2676233
Jerry Swan <swanj21@gmail.com>
2019-11-24 16:55:54 -0600

    Initial libgdx commit

---
+++ android/src/com/swanj21/starfishcollector/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.swanj21.starfishcollector;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.swanj21.starfishcollector.StarfishCollector;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new StarfishCollector(), config);
+	}
+}
